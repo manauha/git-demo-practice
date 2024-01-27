fe321d92dc460a8543015fcab7e9324cf35e45c1
manauha <manauha@gmail.com>
2024-01-27 15:06:50 +0000

    commit2

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,9 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("My Name is Prime");
+
+        for(int i = 0; i <= 5; i++){
+            System.out.println(i);
+        }
     }
 }
